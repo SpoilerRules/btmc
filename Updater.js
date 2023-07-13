8795f801da3ffdbcc0d50c5f0bdb3fbea21b87f6
Spoiler <55200670+SpoilerRules@users.noreply.github.com>
2023-07-13 05:37:54 +0200

    Create Updater.js

---
+++ Updater.js
@@ -0,0 +1,10 @@
+/*
+ * Custom Restrictive License
+ * 
+ * This script file (the "Software") is subject to the terms and conditions of the Custom Restrictive License.
+ * You should have received a copy of the license along with this file. If not, please visit:
+ * https://github.com/SpoilerRules/btmc/blob/main/LICENSE.md
+ * 
+ * All rights reserved. Unauthorized use, copying, or reproduction of any part of this Software is strictly prohibited.
+ */
+// soon™
