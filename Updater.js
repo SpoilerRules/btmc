f0a75742d168444a0578ef6b18fd5c59546b1ecd
Spoiler <55200670+SpoilerRules@users.noreply.github.com>
2023-07-13 06:00:00 +0200

    no shit sherlock

--- Updater.js
+++ Updater.js
@@ -2,7 +2,7 @@
  * Custom Restrictive License
  * 
  * This script file (the "Software") is subject to the terms and conditions of the Custom Restrictive License.
- * You should have received a copy of the license along with this file. If not, please visit:
+ * For the full text of the license, please visit:
  * https://github.com/SpoilerRules/btmc/blob/main/LICENSE.md
  * 
  * All rights reserved. Unauthorized use, copying, or reproduction of any part of this Software is strictly prohibited.
