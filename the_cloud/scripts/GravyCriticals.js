340923ed9df9f6299e4c4d2a1975223e3e0dc2c2
Spoiler <55200670+SpoilerRules@users.noreply.github.com>
2023-08-18 22:13:20 +0200

    life of quality update GravyCriticals.js
    this is a quick release, things may break. contact 'spoilerrules' on discord for assistance.

--- the_cloud/scripts/GravyCriticals.js
+++ the_cloud/scripts/GravyCriticals.js
@@ -8,22 +8,27 @@
  * All rights reserved. Unauthorized use, copying, or reproduction of any part of this Software is strictly prohibited.
  */
 //@ author Spoili
-//@ version 2.6
-//@ description YEP
-var critz = rise.registerModule(rise.getModule("Interface").getSetting("Remove Spaces") ? "GravyCriticals" : "Gravy Criticals", "Enables you to critically hit your opponent when it's time to do some critical damage");
-script.handle("onUnload", function () {critz.unregister();})
+//@ version 2.61
+//@ description Enables you to critically hit your opponent when it's time to do some critical damage
+var critz = rise.registerModule(rise.getModule("Interface").getSetting("Remove Spaces") ? "GravyCriticals" : "Gravy Criticals", "Execute the \".script reload\" command if the settings do not appear.");
+script.handle("onUnload", function () {critz.unregister();});
 
 var currentTarget = null;
 var jumped = false;
+var jumpOrHopMode = null;
+var ticks = 0;
 
-var Aura = rise.getModule("Kill Aura");
+var spaceCheck = rise.getModule("Interface").getSetting("Remove Spaces");
+
+var Aura = spaceCheck ? rise.getModule("Kill Aura") : rise.getModule("KillAura")
 var Criticals = rise.getModule("Criticals");
 var Speed = rise.getModule("Speed");
 var Fly = rise.getModule("Flight");
-var Jumpy = rise.getModule("Long Jump");
+var Jumpy = spaceCheck ? rise.getModule("Long Jump") : rise.getModule("LongJump");
 
-critz.registerSetting("mode", "Mode", "Jump", "Jump", "Packet Vanilla", "BlocksMC", "Fan", "Old Hypixel");
+critz.registerSetting("mode", "Mode", "Jump", "Jump", "Hop", "Packet Vanilla", "BlocksMC", "Test", "Old Hypixel");
 critz.registerSetting("number", "Delay", 600, 0, 3000, 0.1);
+critz.registerSetting("number", "Hop Height", 0.34, 0.10, 0.80, 0.01);
 critz.registerSetting("boolean", "Smooth Camera", false);
 critz.registerSetting("number", "Landing Motion", 0, 0.01, 8, 0.05);
 critz.registerSetting("number", "Landing Motion Y", 0.06, 0.01, 6, 0.05);
@@ -64,21 +69,23 @@ function timeReset() {time = rise.getSystemMillis();}
 function zero() {time = -1;}
 
 critz.handle("onAttack", function (ag) {
-  var target = ag.getTarget(); currentTarget = target;
+  var target = ag.getTarget(); currentTarget = target; jumpOrHopMode = critz.getSetting("Mode") === "Jump" || critz.getSetting("Mode") === "Hop";
   var posX = player.getPosition().getX();
   var posY = player.getPosition().getY();
   var posZ = player.getPosition().getZ();
 
-  if (input.isKeyBindJumpDown() || Fly.isEnabled() || Criticals.isEnabled() || Speed.isEnabled() || Jumpy.isEnabled() || !player.isOnGround()) return;
-  if (player.isInWater() || player.isInLava() || player.isInWeb() || player.isOnLadder()) return;
+  if (input.isKeyBindJumpDown() || Fly.isEnabled() || Criticals.isEnabled() || Speed.isEnabled() || Jumpy.isEnabled()) return;
+  if (!player.isOnGround() || player.isInWater() || player.isInLava() || player.isInWeb() || player.isOnLadder()) return;
   if (critz.getSetting("Aura Only") ? !(Aura.isEnabled() && world.getTargetEntity(Aura.getSetting("Range"))) : !target) return;
   if (critz.getSetting("Sword & Axe Only") && !(player.isHoldingSword() || isHoldingAxe())) return;
   if (critz.getSetting("No Move Check") && player.isMoving() && !critz.getSetting("Force No Move")) return;
   if (critz.getSetting("Sprint Check") && player.isSprinting() && !critz.getSetting("Force No Sprint")) return;
 
-  if (critz.getSetting("Mode") === "Jump") player.jump(), jumped = true;
-  else {
-    if (!hasTimePassed(critz.getSetting("Delay"))) return;
+   if (jumpOrHopMode) {
+     critz.getSetting("Mode") === "Jump" ? player.jump() : player.setMotionY(critz.getSetting("Hop Height"));
+     jumped = true;
+   } else {                                                        /*  !!!  extra protection !!! */
+    if (!hasTimePassed(critz.getSetting("Delay")) || !player.isCollidedVertically() || player.isCollidedHorizontally()) return;
 
     if (critz.getSetting("Force No Move") && player.isMoving()) player.stop();
     if (critz.getSetting("Force No Sprint") && player.isSprinting()) player.setSprinting(false);
@@ -92,16 +99,10 @@ critz.handle("onAttack", function (ag) {
         packet.sendPosition(posX, posY + 3e-14, posZ, true);
         packet.sendPosition(posX, posY + 8e-15, posZ, false);
         break;
-      case "Fan":
-        var numbers = [0.05101, 0.01601, 0.0301, 0.00101];
-        for (var i = 0; i < numbers.length; i++) {
-          var randomIndex = Math.floor(Math.random() * numbers.length);
-          var temp = numbers[i];
-          numbers[i] = numbers[randomIndex];
-          numbers[randomIndex] = temp;
-        }
-        packet.sendPosition(posX, posY + numbers[2], posZ, false);
-        packet.sendPosition(posX, posY, posZ, false);
+      case "Test":
+        ticks++
+        if (ticks < 12) packet.sendPosition(posX, posY, posZ, false);
+        if (ticks > 14) packet.sendPosition(posX, posY + 0.06000831, posZ, false), packet.sendPosition(posX, posY, posZ, false), ticks = 0;
         break;
       case "Old Hypixel":
         packet.sendPosition(posX, posY, posZ, false);
@@ -114,7 +115,7 @@ critz.handle("onAttack", function (ag) {
 });
 
 critz.handle("onPreMotion", function () {
-  if (critz.getSetting("Mode") === "Jump") {
+  if (jumpOrHopMode) {
     if (player.isOnGround()) jumped = false;
     if (jumped && critz.getSetting("Smooth Camera")) render.smoothCamera();
   } else jumped = false;
@@ -122,6 +123,6 @@ critz.handle("onPreMotion", function () {
 });
 
 critz.handle("onStrafe", function () {
-  if (critz.getSetting("Landing Motion") === 0) return; if (critz.getSetting("Mode") !== "Jump") return; if (!jumped) return; if (input.isKeyBindJumpDown() || Fly.isEnabled() || Criticals.isEnabled() || Speed.isEnabled() || Jumpy.isEnabled()) return; if (critz.getSetting("Aura Only") ? !(Aura.isEnabled() && world.getTargetEntity(Aura.getSetting("Range"))) : !currentTarget) return; if (player.isOnGround()) return; if (critz.getSetting("Sword & Axe Only") && !(player.isHoldingSword() || isHoldingAxe())) return; if (critz.getSetting("No Move Check") && player.isMoving() && !critz.getSetting("Force No Move")) return;
+  if (critz.getSetting("Landing Motion") === 0) return; if (!jumpOrHopMode) return; if (!jumped) return; if (input.isKeyBindJumpDown() || Fly.isEnabled() || Criticals.isEnabled() || Speed.isEnabled() || Jumpy.isEnabled()) return; if (critz.getSetting("Aura Only") ? !(Aura.isEnabled() && world.getTargetEntity(Aura.getSetting("Range"))) : !currentTarget) return; if (player.isOnGround()) return; if (critz.getSetting("Sword & Axe Only") && !(player.isHoldingSword() || isHoldingAxe())) return; if (critz.getSetting("No Move Check") && player.isMoving() && !critz.getSetting("Force No Move")) return;
   if (player.getMotion().getY() < critz.getSetting("Landing Motion Y")) player.setMotionY(player.getMotion().getY() - critz.getSetting("Landing Motion"));
 });
