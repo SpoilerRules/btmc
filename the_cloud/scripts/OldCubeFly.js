2cc20d3b2d243353dc8cc7f5c135c61427e4349f
Spoiler <55200670+SpoilerRules@users.noreply.github.com>
2023-08-19 14:26:01 +0200

    OldCubeFly.js remove license

--- the_cloud/scripts/OldCubeFly.js
+++ the_cloud/scripts/OldCubeFly.js
@@ -1,12 +1,3 @@
-/*
- * Custom Restrictive License
- * 
- * This script file (the "Software") is subject to the terms and conditions of the Custom Restrictive License.
- * For the full text of the license, please visit:
- * https://github.com/SpoilerRules/btmc/blob/main/LICENSE.md
- * 
- * All rights reserved. Unauthorized use, copying, or reproduction of any part of this Software is strictly prohibited.
- */
 //@ author Spoili, CzechHek
 //@ version 6.9
 //@ description YEP
